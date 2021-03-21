fdb6ab14292efe754b3e4b5146c7ce2c61c57a61
Mariana Fernandes Ribeiro <mariana.ribeiro@pravaler.com.br>
2021-03-21 16:51:02 -0300

    updating port to env

--- lib/talent_fest_api/application.ex
+++ lib/talent_fest_api/application.ex
@@ -8,7 +8,11 @@ defmodule TalentFestApi.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      Plug.Cowboy.child_spec(scheme: :http, plug: TalentFestApi.Router, options: [port: 4001])
+      Plug.Cowboy.child_spec(
+        scheme: :http,
+        plug: TalentFestApi.Router,
+        options: [port: System.get_env("PORT")]
+      )
     ]
 
     opts = [strategy: :one_for_one, name: TalentFestApi.Supervisor]
