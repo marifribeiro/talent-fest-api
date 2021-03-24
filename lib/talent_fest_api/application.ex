21b263f0843e4dcf98ecf1766cb864a37992eda7
Mariana Fernandes Ribeiro <mariana.ribeiro@pravaler.com.br>
2021-03-24 19:21:04 -0300

    updating port

--- lib/talent_fest_api/application.ex
+++ lib/talent_fest_api/application.ex
@@ -11,7 +11,7 @@ defmodule TalentFestApi.Application do
       Plug.Cowboy.child_spec(
         scheme: :http,
         plug: TalentFestApi.Router,
-        options: [port: {:system, "PORT"}]
+        options: [port: String.to_integer(System.get_env("PORT"))]
       )
     ]
 
